e004a6d4161a67a25154a907160153b3f8f11cfa
Alex Melnyk <melnykog@gmail.com>
2015-12-04 08:09:57 +0300

    refs #40731#change-22772

--- plugins/map/app/controllers/index_controller.rb
+++ plugins/map/app/controllers/index_controller.rb
@@ -13,15 +13,16 @@ class IndexController < ApplicationController
     # @project variable must be set before calling the authorize filter
     @project = Project.find(params[:project_id])
     @settings = TabSetting.get_settings(@project.id)
-    c = 0
-    custom_fields = @project.custom_field_values.each do |cf|
-      if cf.custom_field.name.to_s.eql? "project_geo"
-        @project_geo = cf.to_s;
-        end
-      if cf.custom_field.name.to_s.eql? "Workpack_id"
-        @workpack_id = cf.to_s;
-      end
-    end
+    # c = 0
+    # custom_fields = @project.custom_field_values.each do |cf|
+    #   if cf.custom_field.name.to_s.eql? "project_geo"
+    #     @project_geo = cf.to_s;
+    #     end
+    #   if cf.custom_field.name.to_s.eql? "Workpack_id"
+    #     @workpack_id = cf.to_s;
+    #   end
+    # end
+    @workpack_id = @settings['map']['workpack_id']
     @userData = User.current.to_json
   end
 
